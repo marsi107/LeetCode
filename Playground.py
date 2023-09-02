95ec8e40e2695d36300cdb5b4b6e2ab3334e1b74
Rubén <98332317+marsi107@users.noreply.github.com>
2023-09-02 19:57:50 +0200

    update gitignore

--- Playground.py
+++ Playground.py
@@ -1,1 +1,15 @@
-import os
+def countdown(n):
+    while n > 0:
+        yield n
+        n -= 1
+
+# Create a generator object
+cd = countdown(5)
+
+# Iterating over the generator
+for num in cd:
+    print(num)
+
+print(__name__)
+import this
+import antigravity
